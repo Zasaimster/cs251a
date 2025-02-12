1df0064dd89453121caf414a29be64f72057b91a
Maxwell Jung <jungmaxwell@gmail.com>
2025-02-12 21:57:54 +0000

    move hw2 configs

--- hw2/benchmark.sh
+++ hw2/benchmark.sh
@@ -5,5 +5,14 @@ WORKSPACE=/workspaces/cs251a # Max's config
 # WORKSPACE=/root/cs251a # Saim's config
 GEM5_DIR=$WORKSPACE/gem5
 GEM5_X86=$GEM5_DIR/build/X86/gem5.opt
+HW2_DIR=$WORKSPACE/hw2
+CONFIG_DIR=$HW2_DIR/configs
 
-$GEM5_X86 ${GEM5_DIR}/configs/deprecated/example/se.py --cmd=${WORKSPACE}/build/hw2/test
+$GEM5_X86 \
+    --outdir ${HW2_DIR}/m5out \
+    ${CONFIG_DIR}/se_8width.py \
+    --cmd=${WORKSPACE}/build/hw2/test \
+    --cpu-type=DerivO3CPU \
+    --caches \
+    --l1d_size=64KiB \
+    --l1i_size=64KiB \
